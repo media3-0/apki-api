fd2205702aa0d8788a0e912107117b1e1bdd3199
Gawlas, Jakub <jakub.gawlas@sap.com>
2017-02-22 22:53:42 +0100

    Fixes after review

--- src/core/logger/logger.js
+++ src/core/logger/logger.js
@@ -7,19 +7,15 @@ import { Config } from './config';
 
 
 function init(cfg: Config) {
-
-  // Maximum logs level to logging,
   winston.level = cfg.level;
-
   if (cfg.logToFile) {
     const TRANSPORT_FILE_OPTIONS = {
       filename: cfg.logPath,
       handleExceptions: true,
       humanReadableUnhandledException: true,
     };
-    process.env.LOG_FILE && winston.add(winston.transports.File, TRANSPORT_FILE_OPTIONS);
+    winston.add(winston.transports.File, TRANSPORT_FILE_OPTIONS);
   }
-
 }
 
 export default init;
