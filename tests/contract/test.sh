66f29b5fb18c4593c5dd40854ee41f885f6c2823
Gawlas, Jakub <jakub.gawlas@sap.com>
2017-03-07 22:16:25 +0100

    Ping server before run test

--- tests/contract/test.sh
+++ tests/contract/test.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-if [ "$1" == "-u" ]; then
-  # update snapshots
-  echo "UPDATE"
-  node --harmony-async-await node_modules/.bin/jest -u
+count=10                              # Maximum number to try.
+while [[ $count -ne 0 ]] ; do
+  ping -c 1 api                          # Try once.
+  rc=$?
+  if [[ $rc -eq 0 ]] ; then
+    count=1                      # If okay, flag to exit loop.
+  else
+    sleep 5
+  fi
+  count=count-1                  # So we don't go forever.
+done
+
+if [[ $rc -eq 0 ]] ; then                  # Make final determination.
+
+  if [ "$1" == "-u" ]; then
+    # update snapshots
+    echo "UPDATE"
+    node --harmony-async-await node_modules/.bin/jest -u
+  else
+    echo "NO UPDATE"
+    node --harmony-async-await node_modules/.bin/jest
+  fi
+
 else
-  echo "NO UPDATE"
-  node --harmony-async-await node_modules/.bin/jest
+    echo "Tests failed."
 fi
